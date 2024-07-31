b8621558981aa988d6033839b3654713440592e8
Zak G <zakgriffith@gmail.com>
2024-07-31 16:21:07 -0600

    Welcome.tsx typo fix

--- packages/nextjs/components/homepage/Welcome.tsx
+++ packages/nextjs/components/homepage/Welcome.tsx
@@ -5,9 +5,9 @@ const Welcome = () => {
     <div className="md:text-base text-[0.8rem] border rounded-xl p-4 text-center">
       <h1 className="font-bold font-typo-round md:text-2xl text-xl ">BuidlGuild Workshop Streams</h1>
       <p>
-        We&apos;re funding BuidlGuidl members for providing high-impact SpeedRunEtherem / Scaffold-ETH demonstrations,
-        workshops, and hackathons! Members can make withdraws from their stream after meaningfully contibuting to the
-        community.
+        We&apos;re funding BuidlGuidl members for providing high-impact SpeedRunEthereum and Scaffold-ETH
+        demonstrations, workshops, and hackathons! Members can make withdraws from their stream after meaningfully
+        contibuting to the community.
       </p>
     </div>
   );
